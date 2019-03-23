9f3c990a6774eff7ddf98c2f71f3f2a741e0b762
Dan Shapiro <dshapiro@express.com>
2019-03-23 11:15:01 -0400

    get some schema mocking going

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import delay from 'delay';
+import { ApolloClient, HttpLink, InMemoryCache, gql } from 'apollo-boost';
+import { ApolloProvider, graphql } from 'react-apollo';
+import { SchemaLink } from 'apollo-link-schema';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { mount } from 'enzyme';
 
-class App extends Component {
+const typeDefs = `
+  type Query {
+    product(id: String): Product
+  }
+  type Product {
+    name: String
+  }
+`;
+
+const mocks = {
+  Int: () => 1,
+  String: () => 'Mock',
+  Float: () => 2.2,
+  Boolean: () => false,
+  Id: () => 'abc123',
+};
+
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({
+  schema,
+  mocks,
+});
+
+const ApolloMockingProvider = (props) => {
+  const client = new ApolloClient({
+    link: new SchemaLink({ schema }),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      {props.children}
+    </ApolloProvider>
+  );
+}
+
+class Thinger extends Component {
   render() {
-    return (
+    return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.props.data && this.props.data.product && this.props.data.product.name || "no data"}
           </p>
           <a
             className="App-link"
@@ -21,6 +63,25 @@ class App extends Component {
           </a>
         </header>
       </div>
+    );
+  }
+}
+
+const ThingerWithData = graphql(
+  gql`
+    query getProduct {
+      product(id: "07800006") {
+        name
+      }
+    }
+  `)(Thinger);
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloMockingProvider>
+        <ThingerWithData />
+      </ApolloMockingProvider>
     );
   }
 }

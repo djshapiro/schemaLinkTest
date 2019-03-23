9eaa93fd0fe429335dc9fd07556b08e1ba056445
Dan Shapiro <dshapiro@express.com>
2019-03-23 14:47:45 -0400

    the tests dowork

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import delay from 'delay';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { SchemaLink } from 'apollo-link-schema';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { mount } from 'enzyme';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders an enzyme test', async () => {
+  const wrapper = mount(
+    <App />
+  );
+
+  await delay(4000);
+
+  const text = wrapper.text();
+  expect(text).toMatch(/Mock/);
 });
